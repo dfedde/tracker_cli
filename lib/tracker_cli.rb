6e43829293723464e18c1f087662219850b87995
Duncan Fedde <duncan@fedde.us>
2016-01-13 04:29:31 +0000

    update the way this all works

--- lib/tracker_cli.rb
+++ lib/tracker_cli.rb
@@ -3,10 +3,69 @@ require 'bundler/setup'
 
 require 'pivotal-tracker'
 
+#this reducer state object is global so maybey its not
+#the best but it will work for now
+class State
+  class << self
+    attr_accessor :state
+    def reducer &block
+      @reducer = block
+    end
+
+    # IDEA: this could genarate a diff
+    # stack so that you could regress a action
+    def reduce state, action
+      @state = @reducer[state, action]
+      puts "Im here >>#{state.inspect}<< action >>#{action}<<"
+    end
+
+    def send_action action
+      reduce state, action
+    end
+  end
+end
+
+###
+# To use a screen the only arugment that the initaliazer expects
+# is a block that will create a new window the argumaents passed
+# to the block are `[width, height, top, left]`
+# example:
+# ```
+# Screen.new method(:method_that_makes_a_window)
+# ```
+# ```
+# Screen.new { |*args| Curses::Window.new *args )
+# ```
+class Screen
+  attr_accessor :window_maker
+
+  def initialize(window_maker = false, &block)
+    raise ArgumentError.new('provide only a proc or a block') if window_maker && block
+    @window_maker = window_maker || block
+  end
+
+  def add(klass, width: 0, height: 0, top: 0, left: 0, **opts)
+    $stderr.puts "#{klass}  #{[height, width, top, left]}"
+    win = window_maker[height, width, top, left]
+    klass.new(win, opts).draw()
+    win.refresh
+  end
+end
+
+###
+# A pencil is a thing that can draw on a screen
+# to alcomponts my be built with admeny otions as you would like 
+# todo: a pencil shold define a event listner that is used when it is infocus
 class Pensil
 
-  def initialize(window)
+  def add_component klass, **opts
+    screen.add klass, **opts
+  end
+
+  def initialize(window, opts)
+    @screen = Screen.new window.method(:subwin)
     @window = window
+    @opts = opts
   end
 
   def cols
@@ -24,7 +83,11 @@ class Pensil
 
   protected
 
-  attr_reader :window
+  attr_reader :window, :opts
+
+  private
+
+  attr_reader :screen
 end
 
 class SplashScreen < Pensil
@@ -41,22 +104,22 @@ end
 
 class ProjectView < Pensil
 
-  def draw(project)
-    top_message = project.name
+  def draw
+    top_message = State.state[:project].name
     window.setpos(0, (cols - top_message.length) / 2)
     window.addstr top_message
     window.refresh
 
+    width = (cols / 3)
     [CurrentList, BacklogList, IceboxList].map.with_index do |klass, i|
-      width = (cols / 3)
-
-      win = window.class.new(lines-1, width, 1, i * width)
-      pane = klass.new(win, project: project)
-      pane.draw
+      add_component klass,
+        project: State.state[:project],
+        height: lines-1,
+        width: width,
+        top: 1,
+        left: i * width
     end
-
   end
-
 end
 
 class StoryList < Pensil
@@ -77,13 +140,9 @@ class StoryList < Pensil
     @highlight
   end
 
-
-  def initialize(window, project:)
-    @project = project
-    @window = window
-  end
-
-  def draw(stories, title)
+  def draw
+    stories = opts[:stories]
+    title = opts[:title]
     $stderr.puts("drawing")
     window.clear
 
@@ -121,48 +180,56 @@ class StoryList < Pensil
     window.setpos(lines, 0)
     window.addstr("└#{?─*(cols-2)}┘")
 
-    window.refresh
   end
-
-  protected
-
-  attr_reader :project
 
 end
 
-class CurrentList < StoryList
+class CurrentList < Pensil
 
   def draw
-   super get_stories, 'Current'
+    add_component(StoryList, stories: get_stories, label:  'Current')
   end
 
   def get_stories
-    $stderr.puts("get stories")
+    $stderr.puts("get stories current")
     project.iteration(:current).stories
     PivotalTracker::Iteration.current(project).stories
   end
+
+  def project
+    State.state[:project]
+  end
 end
 
-class BacklogList < StoryList
+class BacklogList < Pensil
+
 
   def draw
-   super get_stories, 'BackLog'
+    add_component(StoryList, stories: get_stories, label:  'Backlog')
   end
 
   def get_stories
-    $stderr.puts("get stories")
+    $stderr.puts("get stories back")
     PivotalTracker::Iteration.backlog(project).map{|i| i.stories}.flatten
   end
+
+  def project
+    State.state[:project]
+  end
 end
 
-class IceboxList < StoryList
+class IceboxList < Pensil
 
   def draw
-   super get_stories, "Icebox"
+    add_component(StoryList, stories: get_stories, label:  'Icebox')
   end
 
   def get_stories
-    $stderr.puts("get stories")
+    $stderr.puts("get stories ice")
     @stories = project.stories.all.select{|story| %w(unscheduled).include? story.current_state}
+  end
+
+  def project
+    State.state[:project]
   end
 end
